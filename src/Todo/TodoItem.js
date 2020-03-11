f6e2654f4ed6a911b34873ba80a651ae1a83f25f
Alisa Khaustova <alice.khaustova@gmail.com>
2020-03-11 21:05:53 +0200

    add styles

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import PropTypse from 'prop-types';
 // import TodoItem from './TodoItem'
 
-export default function TodoIteam({todo, index}) {
+
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem',
+    },
+    input: {
+        marginRight: '1rem',
+    }
+}
+function TodoItem({todo, index}) {
     return(
 
-        <li>
-            <strong> {index + 1} </strong>
-            {todo.title}</li>
+        <li style={styles.li}>
+            <span>
+                <input type="checkbox" style={styles.input}/>
+                <strong> {index + 1} </strong>
+                &nbsp;
+                {todo.title}
+            </span>
+            <button className="rm">&times;</button>
+        </li>
+
     )
 }
+
+TodoItem.propTypse = {
+    todos: PropTypse.object.isRequired,
+    index: PropTypse.number,
+}
+
+export default TodoItem
