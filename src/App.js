9ac6c19818218d02b50a5378e88443d6329dab0c
Alisa Khaustova <alice.khaustova@gmail.com>
2020-03-13 15:29:51 +0200

    add jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import TodoList from './Todo/TodoList';
-import Context from './context';
+import React, {useEffect} from 'react'
+import TodoList from './Todo/TodoList'
+import Context from './context'
+import AddTodo from './Todo/AddTodo'
+import Loader from './Loader'
 
 function App() {
-  const [todos, setTodos] = React.useState(
-  [{id: 1, completed: false,title:'Купить хлеб'},
-  {id: 2, completed: false,title:'Купить масло'},
-  {id: 3, completed: false,title:'Купить молоко'},
-  ]
-  );
+  const [todos, setTodos] = React.useState( [] )
+  const [loading, setLoadind] = React.useState(true)
+
+  useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() =>{
+          setTodos(todos)
+        }, 2000)
+        
+      })
+  }, []) 
 
 
   function toggleTodo(id) {
@@ -22,6 +31,16 @@ function App() {
     )
   }
 
+  function addTodo(title) {
+    setTodos(todos.concat([
+        {
+        title,
+        id: Date.now(),
+        completed: false,
+      }
+    ]))
+  }
+
   function removeTodo(id){
     setTodos(todos.filter(todo => todo.id !== id))
   }
@@ -30,8 +49,13 @@ function App() {
     <Context.Provider value={{removeTodo}}>
       <div className='wrapper'>
         <h1>React tutorial</h1>
+        <AddTodo onCreate = {addTodo}/>
 
-        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <p>No todos</p>}
+        {loading && <Loader/>}
+        {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo}/> 
+        ) : (
+        <p>No todos</p>
+        )}
 
       </div>
     </Context.Provider>
